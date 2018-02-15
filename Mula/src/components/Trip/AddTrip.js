7ebcff5e0f7ac5a875c4d1335eda20777122dfbf
AnneloreVB <xnlore@hotmail.com>
2018-02-15 13:25:47 +0100

    addTrip can add subject, start en enddate pick an item (wordt later reisgenoten) een base currency en meerdere extra currencies

--- Mula/src/components/Trip/AddTrip.js
+++ Mula/src/components/Trip/AddTrip.js
@@ -47,8 +47,10 @@ export default class AddTrip extends Component{
             selectedStartDate: "",
             selectedEndDate: "",
             selectedItems: [],
-            currencies: [],
-            selectedCurrencies: []
+            selectedCurrencies: [],
+            baseCurrency: "EUR",
+            rates: Object,
+            loadRates: true
 
             
         };
@@ -58,6 +60,8 @@ export default class AddTrip extends Component{
       componentDidMount() {
         selectedStartDate = new Date().toDateString
         selectedEndDate = new Date().toDateString
+         
+        this.getExchangeRates();
         
       }
     onSelectedItemsChange = selectedItems => {
@@ -65,11 +69,16 @@ export default class AddTrip extends Component{
         console.log(selectedItems);
         
       };
-      onSelectedCurrencyChange = selectedCurrencies => {
-        this.setState({ selectedItems });
-        console.log(selectedItems);
+    onSelectedCurrencyChange = selectedCurrencies => {
+        console.log("hallo");
+        this.setState({ selectedCurrencies });
+        console.log(selectedCurrencies);
         
       };
+    onSelectBaseCurrencyChange = baseCurrency => { 
+        this.setState({baseCurrency});
+        console.log(baseCurrency);
+    }
 
       //GET EXCHANGE RATES
     getExchangeRates(){
@@ -79,18 +88,68 @@ export default class AddTrip extends Component{
             .then((data) => this.parseRates(data));
         }
     }
+    getExchangeRatesWithBase(baseCurrency){
+        var url = "https://api.fixer.io/latest?base="+baseCurrency;
+        console.log(this.state.loadRates)
+        if(this.state.loadRates){
+            return fetch(url)
+            .then((resp) => resp.json())
+            .then((data) => this.parseRates(data));
+        }
+        console.log(url);
+        console.log(this.state.rates)
+    }
     
+    parseRates(data){
+        if(this.state.loadRates){
+            this.setState({loadRates:false, rates: data.rates});
+        }
+    }
+    renderValuta(rate){
+        return Object.keys(rate).map((val)=> {
+            var label= val+"("+ rate[val]+")";
+            return(
+                <Picker.Item value={val} label={label} key={val}/>
+            )
+        });
+        
+    }
+    renderValutaToArray(rate){
+        var array = [];
+        return Object.keys(rate).map((val)=> {
+            var label= val+"("+ rate[val]+")";
+            array.push({
+                id: val,
+                name: label
+              })
+            this.setState({currencies: array});
+            
+        });
+        
+    }
+    renderValutaWithoutRate(rate){
+        return Object.keys(rate).map((val)=> {
+            
+            return(
+                <Picker.Item value={val} label={val} key={val}/>
+            )
+        });
+    }
       
     render(){
         const { selectedItems } = this.state;
+        const {selectedCurrencies} = this.state;
+        const { rates } = this.state;
+        const {baseCurrency}= this.state;
+        const {loadRates} = this.state;
         var month =  new Date().getUTCMonth() +1 ;
         var yeara = new Date().getFullYear() +1 ;
         var yearb = new Date().getFullYear()-1 ;
         var yearbefore = ""+ yearb +"-"+ month+ "-"+ new Date().getUTCDate();
         var yearafter = ""+yeara +"-"+ month+ "-"+ new Date().getUTCDate();
         
-        console.log(yearbefore);
-        console.log(yearafter);
+        console.log();
+        
         return(
             <ScrollView style={styles.container}>
             <View >
@@ -159,7 +218,7 @@ export default class AddTrip extends Component{
                 onSelectedItemsChange={this.onSelectedItemsChange}
                 selectText="Pick Items"
                 searchInputPlaceholderText="Search Items..."
-                onChangeInput={ (item)=> console.log(text)}
+                onChangeInput={ (item)=> console.log(item)}
                 tagRemoveIconColor="#CCC"
                 tagBorderColor="#CCC"
                 tagTextColor="#CCC"
@@ -173,7 +232,40 @@ export default class AddTrip extends Component{
                 />
                 
             </View>
+            <View style={[styles.subItem]}>
+                        <Picker style={{flex:.50}}           
+                            onValueChange={currency => this.setState({baseCurrency: currency}) &this.setState({loadRates: true}) & this.getExchangeRatesWithBase(currency) & this.renderValutaToArray(this.state.rates)}
+                            selectedValue={this.state.baseCurrency}>
+                            <Picker.Item value= {this.state.baseCurrency} label={this.state.baseCurrency} key={this.state.baseCurrency}/>
+                            {this.renderValutaWithoutRate(this.state.rates)}
+                        </Picker>
+            </View>
+           
             
+            <View style={{ flex: 1 }}>
+                <MultiSelect
+                hideTags
+                items={this.state.currencies}
+                uniqueKey="id"
+                ref={(component) => { this.multiSelect = component }}
+                selectedItems={selectedCurrencies}
+                onSelectedItemsChange={this.onSelectedCurrencyChange}
+                selectText="Pick your currencies"
+                searchInputPlaceholderText="Search currency..."
+                onChangeInput={ (item)=> console.log(item)}
+                tagRemoveIconColor="#CCC"
+                tagBorderColor="#CCC"
+                tagTextColor="#CCC"
+                selectedItemTextColor="#CCC"
+                selectedItemIconColor="#CCC"
+                itemTextColor="#000"
+                displayKey="name"
+                searchInputStyle={{ color: '#CCC' }}
+                submitButtonColor="#CCC"
+                submitButtonText="Submit"
+                />
+                
+            </View>
             
           
             
