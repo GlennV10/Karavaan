52555e3a8c5142f9b04f6e1f874cf0af16fa0683
GlennV10 <verlindenglenn@hotmail.com>
2018-03-06 13:49:43 +0100

    AddTrip working

--- Mula/src/components/Trip/AddTrip.js
+++ Mula/src/components/Trip/AddTrip.js
@@ -37,12 +37,14 @@ export default class AddTrip extends Component{
             trips: [],
             teller: 0,
             errors: [],
-            paling:[],
+            paling: [],
             company: "",
-            formatCurrencies: []
+            formatCurrencies: [],
+            admin: {}
         };
+    }
 
-
+    componentWillMount() {
     }
 
     componentDidMount() {
@@ -57,101 +59,113 @@ export default class AddTrip extends Component{
             .catch(error => console.log('Error loading teller'));
         console.log("MMMMm" + this.state.teller);
 
-        AsyncStorage.getItem('userName')
-        .then((userName) => {
+        AsyncStorage.getItem('userName').then((userName) => {
             this.setState({ username: userName })
+            this.getPerson();
         })
         .catch(error => console.log('Error loading userName'));
     }
-    
 
+    getPerson() {
+        return fetch('http://193.191.177.73:8080/karafinREST/getPerson/' + this.state.username, {
+            method: 'GET',
+            header: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((response) => {
+            this.setState({ admin: response })
+        })
+        .catch(error => console.log("network/rest error"));
+    }
 
     addTrip() {
-
         this.state.errors = [];
         this.state.paling =[];
-        this.renderPaling();
+        // this.renderPaling();
 
-
-        
         //if(this.isValid()){
-            console.log(this.state.paling)
-            
-            this.formatCurrenciesAPI();
-            console.log(this.state.formatCurrencies)
-            if (this.state.connectionMode == "online") {
-                console.log("ONLINE")
-                
-                
-                return fetch('https://193.191.177.73:8181/karafinREST/addTrip', {
-                    method: 'POST',
-                    header: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        "email": this.state.username,
-                        "tripName": this.state.title,
-                        "startDate": {
-                            "dayOfMonth": parseInt(this.state.selectedStartDate.substring(0, 2)),
-                            "month": parseInt(this.state.selectedStartDate.substring(3, 5)),
-                            "year": parseInt(this.state.selectedStartDate.substring(6))                            
-                        },
-                        "endDate": {
-                            "dayOfMonth": parseInt(this.state.selectedEndDate.substring(0, 2)),
-                            "month": parseInt(this.state.selectedEndDate.substring(3, 5)),
-                            "year": parseInt(this.state.selectedEndDate.substring(6))
-                            
-                        },
-                        "baseCurrency": this.state.baseCurrency,
-                        "rates": this.state.formatCurrencies
-                    })
-                })
-                    .then((res) => res.json())
-                    .then((response) => {
-                        
-                        console.log("added trip successfully: " + responseJson.addTrip_succes);
-                        if (responseJson.addTrip_succes === "true") {
-                            this.moveOn();
-                        }
-                    }).catch(error => console.log("network/rest error"));
-            }
-            else {
-                
-                let trp = {
-                    tripID: this.state.teller,
-                    email: this.state.username,
-                    name: this.state.title,
-                    startDate: this.state.selectedStartDate,
-                    endDate: this.state.selectedEndDate,
-                    users: this.state.selectedItems,
-                    expenseList: [],
-                    baseCurrency: this.state.baseCurrency,
-                    currencies: this.state.formatCurrencies
-                    
-                }
-                AsyncStorage.getItem('trips')
-                    .then(req => JSON.parse(req))
-                    .then((trips) => {
-                        trips.push(trp);
-                        AsyncStorage.setItem('trips', JSON.stringify(trips))
-                            .then(res => console.log(trips))
-                            .catch(error => console.log('Error storing trips'));
-                    })
-                    .catch(error => console.log('Error loading trips'));
-                var tel = parseInt(this.state.teller) + 1;
-                console.log("TEL:" + tel);
-    
-                AsyncStorage.setItem('id_teller', JSON.stringify(tel))
-                    .then(res => console.log(teller))
-                    .catch(error => console.log('Error storing teller BBBB'));
-    
-                this.moveOn();
-            }
-        
-        
+            // if (this.state.connectionMode == "online") {
+            //     console.log("ONLINE")
+
+        let trip = {
+            tripName: this.state.title,
+            startDate: {
+                dayOfMonth: parseInt(this.state.selectedStartDate.substring(0, 2)),
+                month: parseInt(this.state.selectedStartDate.substring(3, 5)),
+                year: parseInt(this.state.selectedStartDate.substring(6))
+            },
+            endDate: {
+                dayOfMonth: parseInt(this.state.selectedEndDate.substring(0, 2)),
+                month: parseInt(this.state.selectedEndDate.substring(3, 5)),
+                year: parseInt(this.state.selectedEndDate.substring(6))
+
+            },
+            baseCurrency: this.state.baseCurrency,
+            rates: this.state.formatCurrencies,
+            participants: [],
+            expenseList: [],
+            categories: [],
+            payments: {}
+        }
+
+        let participant = [];
+        participant.push(this.state.admin);
+        participant.push("ADMIN");
+        trip.participants.push(participant);
+
+        return fetch('http://193.191.177.73:8080/karafinREST/addTrip', {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(trip)
+        })
+        .then((res) => res.json())
+        .then((response) => {
+            console.log(response);
+            this.props.navigation.navigate('TripParticipants', {trip});
+        })
+        .catch(error => console.log("network/rest error"));
+            // }
+            // else {
+            //
+            //     let trp = {
+            //         tripID: this.state.teller,
+            //         email: this.state.username,
+            //         name: this.state.title,
+            //         startDate: this.state.selectedStartDate,
+            //         endDate: this.state.selectedEndDate,
+            //         users: this.state.selectedItems,
+            //         expenseList: [],
+            //         baseCurrency: this.state.baseCurrency,
+            //         currencies: this.state.formatCurrencies
+            //
+            //     }
+            //     AsyncStorage.getItem('trips')
+            //         .then(req => JSON.parse(req))
+            //         .then((trips) => {
+            //             trips.push(trp);
+            //             AsyncStorage.setItem('trips', JSON.stringify(trips))
+            //                 .then(res => console.log(trips))
+            //                 .catch(error => console.log('Error storing trips'));
+            //         })
+            //         .catch(error => console.log('Error loading trips'));
+            //     var tel = parseInt(this.state.teller) + 1;
+            //     console.log("TEL:" + tel);
+            //
+            //     AsyncStorage.setItem('id_teller', JSON.stringify(tel))
+            //         .then(res => console.log(teller))
+            //         .catch(error => console.log('Error storing teller BBBB'));
+            //
+            //     this.moveOn();
+            // }
+
+
         //}
         //alert(this.state.errors)
-        
+
     }
 
     //////////////////////////////////////////////////////////
@@ -166,10 +180,12 @@ export default class AddTrip extends Component{
         console.log(selectedCurrencies);
 
     };
+
     onSelectBaseCurrencyChange = baseCurrency => {
         this.setState({ baseCurrency });
         console.log(baseCurrency);
     }
+
     getExchangeRates() {
         if (this.state.loadRates) {
             return fetch('https://api.fixer.io/latest')
@@ -178,6 +194,7 @@ export default class AddTrip extends Component{
                 .catch(error => console.log("network/fixer error"));
         }
     }
+
     getExchangeRatesWithBase(baseCurrency) {
         console.log("Rates met base wordt uitgevoerd" + baseCurrency)
         var url = "https://api.fixer.io/latest?base=" + baseCurrency;
@@ -191,116 +208,90 @@ export default class AddTrip extends Component{
     }
 
     parseRates(data) {
-        console.log(data.rates)
-        //if (this.state.loadRates) {
         this.setState({ rates: data.rates });
         this.renderValutaToArray(data.rates);
-        //}
     }
-    renderPaling() {
 
+    renderValuta(rate) {
+        return Object.keys(rate).map((val) => {
+            var label = val + "(" + rate[val] + ")";
+
+            return (
+                <Picker.Item value={val} label={label} key={val} />
+            )
+        });
+    }
+
+    renderValutaToArray(rate) {
+        var array = [];
+        return Object.keys(rate).map((val) => {
+            var label = val + "(" + rate[val] + ")";
+            array.push({
+                id: label,
+                name: label,
+                currency: val
+            })
+            this.setState({ currencies: array });
+        });
+    }
+
+    renderValutaWithoutRate(rate) {
+        return Object.keys(rate).map((val) => {
+            return (
+                <Picker.Item value={val} label={val} key={val} />
+            )
+        });
+    }
+
+    renderPaling() {
         console.log("running" + this.state.selectedCurrencies[0])
         for(let i= 0; i < this.state.selectedCurrencies.length; i++){
             for(key of Object.keys(this.state.rates)){
                 if(key === this.state.selectedCurrencies[i]){
-                    
-                   
                     this.state.paling.push({
                         rate: this.state.rates[key],
                         val: key
                     })
                 }
             }
-            
         }
         console.log(this.state.paling)
-
     }
-    renderValuta(rate) {
-        return Object.keys(rate).map((val) => {
-            var label = val + "(" + rate[val] + ")";
-            
-            return (
-                <Picker.Item value={val} label={label} key={val} />
-            )
-        });
 
-    }
-    renderValutaToArray(rate) {
-        var array = [];
-        return Object.keys(rate).map((val) => {
-            var label = val + "(" + rate[val] + ")";
-            
-            array.push({
-                id: label,
-                name: label
-            })
-            this.setState({ currencies: array });
-
-        });
-
-    }
-    renderValutaWithoutRate(rate) {
-        return Object.keys(rate).map((val) => {
-
-            return (
-                <Picker.Item value={val} label={val} key={val} />
-            )
-        });
-    }
     isValid() {
         var res = true;
-           
-                if(this.state.title.length === 0 || this.state.title.length > 15){
-                    this.state.errors.push("please add a valid tripName ")
-                    res = false;
-                }
-                
-                if(this.state.startDate == null ){
-                    this.state.errors.push("please add a valid startDate ")
-                    res = false;
-                }
-                if(this.state.endDate == null){
-                    this.state.errors.push("please add a valid endDate ")
-                    res = false;
-                }
-                
-            return res;
-    }
-    
-    
-    //////////////////////////////////////////////////////////
-    ////////////////////FRIENDS//////////////////////////////
 
-    onSelectedItemsChange = selectedItems => {
-        this.setState({ selectedItems });
-
-
-    };
-    onSelectedGuidesChange = selectedGuides => {
-        this.setState({ selectedGuides });
-        console.log(selectedGuides);
-    } 
-    
-    
-    formatCurrenciesAPI() {
-        console.log(this.state.paling)
-        let formatCurrencies = {};
-        for(currency of this.state.paling) {
-            let key = currency.val;
-            formatCurrencies[key] = currency.rate;
+        if(this.state.title.length === 0 || this.state.title.length > 15){
+            this.state.errors.push("please add a valid tripName ")
+            res = false;
         }
-        console.log(formatCurrencies)
-        this.setState({ formatCurrencies });
+
+        if(this.state.startDate == null ){
+            this.state.errors.push("please add a valid startDate ")
+            res = false;
+        }
+        if(this.state.endDate == null){
+            this.state.errors.push("please add a valid endDate ")
+            res = false;
+        }
+
+        return res;
     }
 
-    
-    
+    async formatCurrenciesAPI() {
+        let formatCurrencies = {};
+        for(currency of this.state.selectedCurrencies) {
+            formatCurrencies[currency] = this.state.rates[currency];
+        }
+        await this.setState({ formatCurrencies });
+        this.addTrip()
+    }
+
     render() {
         const { selectedItems } = this.state;
         const { selectedGuides } = this.state;
         const { selectedCurrencies } = this.state;
-        const {errors} = this.state;
+        const { errors } = this.state;
         const { rates } = this.state;
         const { baseCurrency } = this.state;
         const { loadRates } = this.state;
@@ -311,25 +302,20 @@ export default class AddTrip extends Component{
         var yearbefore = "" + yearb + "-" + month + "-" + new Date().getUTCDate();
         var yearafter = "" + yeara + "-" + month + "-" + new Date().getUTCDate();
 
-
-        console.log();
-
         return (
             <ScrollView style={styles.container}>
-                        
-                
-                        <View style={styles.separator}>
-                            <TextInput
-                                ref="tripName"
-                                placeholder={I18n.t('tripname')}
-                                style={styles.inputField}
-                                underlineColorAndroid="transparent"
-                                placeholderTextColor="#818181"
-                                onChangeText={(text) => console.log(text) & this.setState({ title: text })} />
-                        </View>
-                    
+                <View style={styles.separator}>
+                    <TextInput
+                        ref="tripName"
+                        placeholder={I18n.t('tripname')}
+                        style={styles.inputField}
+                        underlineColorAndroid="transparent"
+                        placeholderTextColor="#818181"
+                        onChangeText={(text) => this.setState({ title: text })} />
+                </View>
+
                 <View style={{ flex: 1, flexDirection: 'row' }}>
-                    <Text>{this.state.selectedStartDate}</Text> 
+                    <Text>{this.state.selectedStartDate}</Text>
                     {/*this.isFieldInError('startDate') && this.getErrorsInField('startDate').map(errorMessage => <Text>{errorMessage}</Text>) */}
 
                     <DatePicker
@@ -345,11 +331,10 @@ export default class AddTrip extends Component{
                         style={[styles.input, styles.datePickerStyle]}
                         onDateChange={(date) => this.setState({ selectedStartDate: date })}
                     />
-                </View >
+                </View>
 
                 <View style={[{ flex: 1, flexDirection: 'row' }, styles.separator]}>
-                <Text>{this.state.selectedEndDate}</Text>    
-                
+                <Text>{this.state.selectedEndDate}</Text>
                     <DatePicker
                         mode='date'
                         format='DD/MM/YYYY'
@@ -363,9 +348,7 @@ export default class AddTrip extends Component{
                         style={[styles.input, styles.datePickerStyle]}
                         onDateChange={(date) => this.setState({ selectedEndDate: date })}
                     />
-
                 </View>
-                
 
                 <View style={[styles.subItem, styles.separator]}>
                     <Text>{I18n.t('tripcurrency')}</Text>
@@ -377,13 +360,12 @@ export default class AddTrip extends Component{
                     </Picker>
                 </View>
 
-
                 <View>
                     <Text style={styles.textfield}>{I18n.t('othercurrency')}</Text>
                     <MultiSelect
                         hideTags
                         items={this.state.currencies}
-                        uniqueKey="id"
+                        uniqueKey="currency"
                         ref={(component) => { this.multiSelect = component }}
                         selectedItems={selectedCurrencies}
                         onSelectedItemsChange={this.onSelectedCurrencyChange}
@@ -402,21 +384,19 @@ export default class AddTrip extends Component{
                         submitButtonColor="#edc14f"
                         submitButtonText={I18n.t('submit')}
                     />
-
-
                 </View>
 
                 {/* <View style={[styles.subItem, styles.separator]}>
                 <Text style ={styles.textfield}>Select your base currency</Text>
-                <Picker style={{flex:.50}}           
+                <Picker style={{flex:.50}}
                     onValueChange={currency => this.setState({baseCurrency: currency}) &this.setState({loadRates: true}) & this.getExchangeRatesWithBase(currency) & this.renderValutaToArray(this.state.rates)}
                     selectedValue={this.state.baseCurrency}>
                     <Picker.Item value= {this.state.baseCurrency} label={this.state.baseCurrency} key={this.state.baseCurrency}/>
                     {this.renderValutaWithoutRate(this.state.rates)}
                 </Picker>
             </View>
-           
-            
+
+
             <View style={[styles.subItem]}>
                 <Text style ={styles.textfield}>Select the other currency you're going to use on the trip</Text>
                 <MultiSelect
@@ -440,15 +420,15 @@ export default class AddTrip extends Component{
                     submitButtonColor="#edc14f"
                     submitButtonText="Submit"
                     color = "#edc14f"
-                    
-                    
-                    
-                /> 
-                
+
+
+
+                />
+
             </View>*/}
                 <Button color="#edc14f"
                     title={I18n.t('savetrip')}
-                    onPress={() => this.addTrip() & console.log("Waiting for backend...")}
+                    onPress={() => this.formatCurrenciesAPI()}
 
                 />
                 <View>
@@ -464,7 +444,6 @@ export default class AddTrip extends Component{
     }
 
 }
-
 
 const styles = StyleSheet.create({
     container: {
@@ -499,8 +478,4 @@ const styles = StyleSheet.create({
         borderBottomWidth: StyleSheet.hairlineWidth,
         marginBottom: 5
     }
-
-
-
-
 });
